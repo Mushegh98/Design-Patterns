e73eed2bf603a1efccd6956b5aa49a1dd10fd79b
Mushegh98 <53486603+Mushegh98@users.noreply.github.com>
2019-10-20 19:01:37 +0400

    Add files via upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Single
+{
+    private static Single ob = null;
+    protected Single()
+    {
+
+    }
+    public static Single Instance()
+    {
+        if(ob==null)
+        {
+            ob = new Single();
+        }
+        
+            return ob;
+        
+    }
+}
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Single sing1 = Single.Instance();
+            Single sing2 = Single.Instance();
+            Console.WriteLine(sing1.GetHashCode());
+            Console.WriteLine(sing2.GetHashCode());
+        }
+    }
+}
